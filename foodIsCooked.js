e6172e5b1f29eb75d99b4ea89032e431bb4d3f23
JCahill <cahilljm@aol.com>
2022-02-11 13:59:45 -0800

    Push solution for foodCooked activity

--- foodIsCooked.js
+++ foodIsCooked.js
@@ -5,12 +5,29 @@
  * @param {string} doneness
  * @returns {boolean} isCooked
  */
-const foodIsCooked = function(kind, internalTemp, doneness) {
+const foodIsCooked = function (kind, internalTemp, doneness) {
   // Write function HERE
-
+  if (kind === 'chicken') {
+    if (internalTemp >= 165) {
+      return (`True - The ${kind}, at ${internalTemp} degrees, is at the correct temperature to be considered done.`)
+    } else if (internalTemp < 165) {
+      return (`False - ${internalTemp} degrees is not warm enough for the ${kind} to be considered done.`)
+    }
+  }
+  if (kind === 'beef') {
+    if ((internalTemp >= 125 && internalTemp < 135) && doneness === 'rare') {
+      return (`True - The ${kind} is warm enough to be ${doneness}.`)
+    } else if ((internalTemp >= 135 && internalTemp < 155) && doneness === 'medium') {
+      return (`True - The ${kind} is warm enough to be ${doneness}.`)
+    } else if ((internalTemp >= 135 && internalTemp >= 155) && doneness === 'well') {
+      return (`True - The ${kind} is warm enough to be ${doneness}.`)
+    } else {
+      return (`False - ${internalTemp} degrees is not the correct temperature to be considered ${doneness}.`)
+    }
+  } else {
+    return (`Not Determined - Information for ${kind} is not available.`)
+  }
 }
-
-
 
 // Test function
 console.log(foodIsCooked('chicken', 90)); // should be false
