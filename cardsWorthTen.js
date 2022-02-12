47c376ad6a75addb80b0de7f7e697e048cdd6dcd
JCahill <cahilljm@aol.com>
2022-02-11 18:37:31 -0800

    Push work on cardsWorthTen and createCardDeck activities

--- cardsWorthTen.js
+++ cardsWorthTen.js
@@ -1,17 +1,68 @@
-const cards = [
-  { val: 2, displayVal: "2", suit: "hearts" },
-  { val: 3, displayVal: "3", suit: "hearts" },
-  { val: 4, displayVal: "4", suit: "hearts" },
-  { val: 5, displayVal: "5", suit: "hearts" },
-  { val: 6, displayVal: "6", suit: "hearts" },
-  { val: 7, displayVal: "7", suit: "hearts" },
-  { val: 8, displayVal: "8", suit: "hearts" },
-  { val: 9, displayVal: "9", suit: "hearts" },
-  { val: 10, displayVal: "10", suit: "hearts" },
-  { val: 10, displayVal: "Jack", suit: "hearts" },
-  { val: 10, displayVal: "Queen", suit: "hearts" },
-  { val: 10, displayVal: "King", suit: "hearts" },
-  { val: 11, displayVal: "Ace", suit: "hearts" }
+const cards = [{
+    val: 2,
+    displayVal: "2",
+    suit: "hearts"
+  },
+  {
+    val: 3,
+    displayVal: "3",
+    suit: "hearts"
+  },
+  {
+    val: 4,
+    displayVal: "4",
+    suit: "hearts"
+  },
+  {
+    val: 5,
+    displayVal: "5",
+    suit: "hearts"
+  },
+  {
+    val: 6,
+    displayVal: "6",
+    suit: "hearts"
+  },
+  {
+    val: 7,
+    displayVal: "7",
+    suit: "hearts"
+  },
+  {
+    val: 8,
+    displayVal: "8",
+    suit: "hearts"
+  },
+  {
+    val: 9,
+    displayVal: "9",
+    suit: "hearts"
+  },
+  {
+    val: 10,
+    displayVal: "10",
+    suit: "hearts"
+  },
+  {
+    val: 10,
+    displayVal: "Jack",
+    suit: "hearts"
+  },
+  {
+    val: 10,
+    displayVal: "Queen",
+    suit: "hearts"
+  },
+  {
+    val: 10,
+    displayVal: "King",
+    suit: "hearts"
+  },
+  {
+    val: 11,
+    displayVal: "Ace",
+    suit: "hearts"
+  }
 ];
 
 /**
@@ -21,7 +72,14 @@ const cards = [
  * @param {array} cards
  * @return {string} displayVal
  */
-const cardsWorthTen = cards => {};
+const cardsWorthTen = cards => {
+
+  let filteredCards = cards.filter((card) => card.val === 10);
+
+  return (filteredCards);
+
+};
 
 console.log(cardsWorthTen(cards));
+
 // should return/log "10, Jack, Queen, King"
--- createCardDeck.js
+++ createCardDeck.js
@@ -4,7 +4,56 @@
  */
 const getDeck = () => {
 
-}
+  const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+  const cards = [];
+  // let suit = [];
+
+
+  for (let i = 0; i < suits.length; i++) {
+    let displayVal
+    let val
+
+    for (let j = 1; j <= 13; j++) {
+      switch (j) {
+        case (j === 1):
+          displayVal = 'Ace';
+          val = 11;
+        case (j > 1 && j <= 10):
+          displayVal = j;
+          val = j;
+        case (j === 11):
+          displayVal = 'Jack';
+          val = 10;
+        case (j === 12):
+          displayVal = 'Queen';
+          val = 10;
+        case (j === 13):
+          displayVal = 'King';
+          val = 10;
+        // default:
+          // displayVal = '';
+          // val = 0;
+      }
+
+      console.log(j);
+      console.log(displayVal);
+      console.log(val);
+      console.log(suits[i])
+
+      cards.push({
+        val,
+        displayVal,
+        suit: suits[i]
+      });
+
+    };
+
+
+
+    console.log(cards)
+
+  };
+};
 
 
 
