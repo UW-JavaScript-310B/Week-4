0f56905ed83c48e9fc85a1943ca76e197b524c89
Arthur Rodrigues <artrodrig3@outlook.com>
2022-02-13 11:38:54 -0800

    Week 4 homework completed.

--- logCardDeck.js
+++ logCardDeck.js
@@ -58,4 +58,10 @@ const cards = [
  *
  * @param {array} deck A deck of cards
  */
-const logCardDeck = deck => {};
+const logCardDeck = deck => {
+    deck.forEach(card => {
+        console.log(`${card.val} ${card.displayVal} ${card.suit}`);
+    });
+};
+
+logCardDeck(cards);
