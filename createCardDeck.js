71b8c57b702075598c2f10a0d18398fd9428707c
RainyCityDiver <cvmembership@posteo.net>
2022-02-15 14:22:53 -0800

    createCardDeck.js tested and working.

--- createCardDeck.js
+++ createCardDeck.js
@@ -3,7 +3,47 @@
  * @returns {Array} deck - a deck of cards
  */
 const getDeck = () => {
+  const suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+  let deck = [];
+  
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 1; j <= 13; j++) {
+      let displayVal = '';
+      let val = 0;
 
+      switch (j) {
+
+        case (1):
+          displayVal = "Ace"
+          val = 11;
+          break;
+        case (11):
+          displayVal = "Jack";
+          val = 10;
+          break;
+        case (12):
+          displayVal = "Queen";
+          val = 10;
+          break;
+        case (13):
+          displayVal = "King";
+          val = 10;
+          break;        
+        default:
+          displayVal = String(j);
+          val = j;
+          break;
+      }
+
+      deck.push({
+        val: val,
+        displayVal: displayVal,
+        suit: suits[i],
+      })
+
+    }
+  } 
+  return deck;  
 }
 
 
