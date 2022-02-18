666e3f0dc5cbda7573ac96f5e10f082090bebd55
Ryan Ku <rjhk@uw.edu>
2022-02-17 16:00:10 -0800

    finished all assignments

--- createCardDeck.js
+++ createCardDeck.js
@@ -32,18 +32,18 @@ const getDeck = () => {
 
 
 // CHECKS
-const deck = getDeck();
-console.log(`Deck length equals 52? ${deck.length === 52}`);
+// const deck = getDeck();
+// console.log(`Deck length equals 52? ${deck.length === 52}`);
 
-const randomCard = deck[Math.floor(Math.random() * 52)];
+// const randomCard = deck[Math.floor(Math.random() * 52)];
 
-const cardHasVal = randomCard && randomCard.val && typeof randomCard.val === 'number';
-console.log(`Random card has val? ${cardHasVal}`);
+// const cardHasVal = randomCard && randomCard.val && typeof randomCard.val === 'number';
+// console.log(`Random card has val? ${cardHasVal}`);
 
-const cardHasSuit = randomCard && randomCard.suit && typeof randomCard.suit === 'string';
-console.log(`Random card has suit? ${cardHasSuit}`);
+// const cardHasSuit = randomCard && randomCard.suit && typeof randomCard.suit === 'string';
+// console.log(`Random card has suit? ${cardHasSuit}`);
 
-const cardHasDisplayVal = randomCard &&
-  randomCard.displayVal &&
-  typeof randomCard.displayVal === 'string';
-console.log(`Random card has display value? ${cardHasDisplayVal}`);
+// const cardHasDisplayVal = randomCard &&
+//   randomCard.displayVal &&
+//   typeof randomCard.displayVal === 'string';
+// console.log(`Random card has display value? ${cardHasDisplayVal}`);
